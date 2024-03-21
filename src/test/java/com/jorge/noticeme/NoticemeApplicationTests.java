26e2283126a9a709893bcb1fdcce14383468498e
Jorge Lugo <jlugo@hey.com>
2024-03-21 15:50:36 -0400

    Initial commit

---
+++ src/test/java/com/jorge/noticeme/NoticemeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jorge.noticeme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoticemeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
